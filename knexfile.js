d768b6ccdee217cdb123d5a98f52f91bd720605d
David Horstman <davidzilla27@gmail.com>
2020-06-18 00:17:14 -0400

    Fix a typo

--- knexfile.js
+++ knexfile.js
@@ -30,17 +30,18 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-  },
-  useNullAsDefault: true,
-  migrations: {
-    directory: "./data/migrations",
-  },
-  seeds: { directory: "./data/seeds" },
-  pool: {
-    min: 2,
-    max: 10,
-    afterCreate: (conn, done) => {
-      conn.run("PRAGMA foreign_keys = ON", done);
+
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: { directory: "./data/seeds" },
+    pool: {
+      min: 2,
+      max: 10,
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
     },
   },
 };
