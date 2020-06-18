57ba228013a13fdd4a13d285bac8628c75d73018
David Horstman <davidzilla27@gmail.com>
2020-06-18 00:11:33 -0400

    Prepare for postgres migration

--- knexfile.js
+++ knexfile.js
@@ -27,4 +27,20 @@ module.exports = {
       },
     },
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./data/migrations",
+  },
+  seeds: { directory: "./data/seeds" },
+  pool: {
+    min: 2,
+    max: 10,
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+    },
+  },
 };
