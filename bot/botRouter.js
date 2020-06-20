2e6041aa9cb270edf9b47d6ad658474664ae63b9
David Horstman <davidzilla27@gmail.com>
2020-06-20 18:23:06 -0400

    Tests for botRouter general functionality

--- bot/botRouter.js
+++ bot/botRouter.js
@@ -2,27 +2,43 @@ const router = require("express").Router();
 const baseUrl = "https://devdesk-queue-2-herokuapp.com/api";
 const config = require("../config/serverInfo");
 const crypto = require("crypto");
+const tsscmp = require("tsscmp");
 
 router.use(verifySignature);
+router.use(function respondToChallenge(req, res, next) {
+  const { challenge } = req.body;
+  challenge ? res.status(200).json({ challenge }) : next();
+});
 
-router.post("/events_verify", (req, res) => {
-  res.status(200).json({ challenge: req.challenge });
+router.post("/events", (req, res) => {
+  console.log(req.body);
+  res.status(204).end();
+});
+
+router.post("/testing", (req, res) => {
+  res.status(204).end();
 });
 
 /*----------------------------------------------------------------------------*/
 /* Middleware
 /*----------------------------------------------------------------------------*/
+
+//https://fireship.io/snippets/verify-slack-api-signing-signature-node
 function verifySignature(req, res, next) {
-  const signature = req.headers["x-slack-signature"];
-  const timestamp = req.headers["x-slack-request-timestamp"];
-  const hmac = crypto.createHmac("sha256", config.SIGNING_SECRET);
-  const [version, hash] = signature.split("=");
+  const slackSigningSecret = config.SIGNING_SECRET;
+  const requestSignature = String(req.headers["x-slack-signature"]);
+  const requestTimestamp = req.headers["x-slack-request-timestamp"];
+  if (Math.abs(Number(Date.now() / 1000) - Number(requestTimestamp)) > 60 * 5) {
+    return res.status(403).json({ message: "Invalid timestamp" });
+  }
+  const hmac = crypto.createHmac("sha256", slackSigningSecret);
+  const [version, hash] = requestSignature.split("=");
+  const base = `${version}:${requestTimestamp}:${JSON.stringify(req.body)}`;
+  hmac.update(base);
 
-  hmac.update(`${version}:${timestamp}:${req.rawBody}`);
-
-  hmac.digest("hex") === hash
+  tsscmp(hash, hmac.digest("hex"))
     ? next()
-    : res.status(403).json({ message: "Invalid slack signature" });
+    : res.status(403).json({ message: "Invalid signature" });
 }
 
 module.exports = router;
