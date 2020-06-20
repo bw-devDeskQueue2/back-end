ec1a08f53388b16899f2028d04be89d317d3c7a9
David Horstman <davidzilla27@gmail.com>
2020-06-20 17:07:54 -0400

    Add slack signature validation

--- bot/botRouter.js
+++ bot/botRouter.js
@@ -1,8 +1,28 @@
 const router = require("express").Router();
 const baseUrl = "https://devdesk-queue-2-herokuapp.com/api";
+const config = require("../config/serverInfo");
+const crypto = require("crypto");
+
+router.use(verifySignature);
 
 router.post("/events_verify", (req, res) => {
   res.status(200).json({ challenge: req.challenge });
 });
+
+/*----------------------------------------------------------------------------*/
+/* Middleware
+/*----------------------------------------------------------------------------*/
+function verifySignature(req, res, next) {
+  const signature = req.headers["x-slack-signature"];
+  const timestamp = req.headers["x-slack-request-timestamp"];
+  const hmac = crypto.createHmac("sha256", config.SIGNING_SECRET);
+  const [version, hash] = signature.split("=");
+
+  hmac.update(`${version}:${timestamp}:${req.rawBody}`);
+
+  hmac.digest("hex") === hash
+    ? next()
+    : res.status(403).json({ message: "Invalid slack signature" });
+}
 
 module.exports = router;
