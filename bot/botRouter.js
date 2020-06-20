573195c74903d170719f13e1e319e607a8af286c
David Horstman <davidzilla27@gmail.com>
2020-06-20 17:48:33 -0400

    Maybe fixed timestamp

--- bot/botRouter.js
+++ bot/botRouter.js
@@ -25,9 +25,9 @@ function verifySignature(req, res, next) {
   const requestSignature = String(req.headers["x-slack-signature"]);
   const requestTimestamp = req.headers["x-slack-request-timestamp"];
   console.log("timestamp", requestTimestamp);
-  if (Math.abs(Number(Date.now()) - Number(requestTimestamp)) > 60 * 5 * 1000) {
+  if (Math.abs(Number(Date.now()/1000) - Number(requestTimestamp)) > 60 * 5) {
     console.log("now", Date.now());
-    console.log(Math.abs(Number(Date.now()) - Number(requestTimestamp))); // return res.status(403).json({ message: "Invalid timestamp" });
+    console.log(Math.abs(Number(Date.now()/1000) - Number(requestTimestamp))); // return res.status(403).json({ message: "Invalid timestamp" });
   }
   const hmac = crypto.createHmac("sha256", slackSigningSecret);
   const [version, hash] = requestSignature.split("=");
