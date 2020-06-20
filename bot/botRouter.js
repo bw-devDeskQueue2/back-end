4b4b829afd205e7235b8059d90cc97da80e0eb3a
David Horstman <davidzilla27@gmail.com>
2020-06-20 17:45:14 -0400

    Still chasing timestamp

--- bot/botRouter.js
+++ bot/botRouter.js
@@ -25,7 +25,8 @@ function verifySignature(req, res, next) {
   const requestSignature = String(req.headers["x-slack-signature"]);
   const requestTimestamp = req.headers["x-slack-request-timestamp"];
   console.log("timestamp", requestTimestamp);
-  if (Math.abs(Date.now() - Number(requestTimestamp)) > 60 * 5 * 1000) {
+  if (Math.abs(Number(Date.now()) - Number(requestTimestamp)) > 60 * 5 * 1000) {
+    console.log("now", Date.now());
     console.log("invalid timestamp"); // return res.status(403).json({ message: "Invalid timestamp" });
   }
   const hmac = crypto.createHmac("sha256", slackSigningSecret);
