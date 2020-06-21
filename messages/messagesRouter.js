cfbd4467ed7d4d4819fc10c8249485456597a3be
David Horstman <davidzilla27@gmail.com>
2020-06-21 16:12:34 -0400

    Require queue router in server instead of as a subroute of ticketsRouter

--- messages/messagesRouter.js
+++ messages/messagesRouter.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const Messages = require("./messagesModel");
-const { catchAsync, AppError } = require("../config/errors");
+const { catchAsync } = require("../config/errors");
 
 router.get(
   "/",
   catchAsync(async (req, res) => {
     const { id } = req.ticket;
-    const { id: sender_id } = req.data;
     res.status(200).json(await Messages.getTicketMessages(id));
   })
 );
