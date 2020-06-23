92cdd887038449c81049685efe2b06ec1ae57acf
David Horstman <davidzilla27@gmail.com>
2020-06-23 00:02:12 -0400

    Registration modal

--- bot/modals.js
+++ bot/modals.js
@@ -1,45 +1,77 @@
 module.exports = {
-  demo: {
+  register: {
     type: "modal",
     title: {
       type: "plain_text",
-      text: "Modal title",
+      text: "Register",
     },
     blocks: [
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: "It's Block Kit...but _in a modal_",
-        },
-        block_id: "section1",
-        accessory: {
-          type: "button",
-          text: {
-            type: "plain_text",
-            text: "Click me",
-          },
-          action_id: "button_abc",
-          value: "Button value",
-          style: "danger",
-        },
-      },
+      // {
+      //   type: "section",
+      //   text: {
+      //     type: "mrkdwn",
+      //     text: "It's Block Kit...but _in a modal_",
+      //   },
+      //   block_id: "section1",
+      //   accessory: {
+      //     type: "button",
+      //     text: {
+      //       type: "plain_text",
+      //       text: "Click me",
+      //     },
+      //     action_id: "button_abc",
+      //     value: "Button value",
+      //     style: "danger",
+      //   },
+      // },
+
       {
         type: "input",
         label: {
           type: "plain_text",
-          text: "Input label",
+          text: "Username",
         },
         element: {
           type: "plain_text_input",
-          action_id: "input1",
+          action_id: "input_username",
           placeholder: {
             type: "plain_text",
-            text: "Type in here",
+            text: "Enter your desired username",
           },
           multiline: false,
         },
         optional: false,
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "input",
+            label: {
+              type: "plain_text",
+              text: "Password",
+            },
+            element: {
+              type: "password_input",
+              action_id: "input_password",
+              multiline: false,
+            },
+            optional: false,
+          },
+          {
+            type: "input",
+            label: {
+              type: "plain_text",
+              text: "Confirm Password",
+            },
+            element: {
+              type: "password_input",
+              action_id: "input_password_confirmation",
+              multiline: false,
+            },
+            optional: false,
+          },
+        ],
       },
     ],
     close: {
@@ -51,6 +83,6 @@ module.exports = {
       text: "Save",
     },
     private_metadata: "Shhhhhhhh",
-    callback_id: "view_identifier_12",
+    callback_id: "registration_form",
   },
 };
