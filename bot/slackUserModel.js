3b73ebb806f7f054cdee40851928119ecc56a3eb
David Horstman <davidzilla27@gmail.com>
2020-06-24 18:16:19 -0400

    Return null instead of undefined for nonexistent user

--- bot/slackUserModel.js
+++ bot/slackUserModel.js
@@ -1,7 +1,9 @@
 const knex = require("../data/dbConfig");
 
 function getUser(query) {
-  return knex("slack_users").where(query).first();
+  return knex("slack_users")
+    .where(query)
+    .then(results => (results.length === 0 ? null : results[0]));
 }
 
 module.exports = { getUser };
