0f5234dfc9b635b1da48a85198c62835d827d3cc
David Horstman <davidzilla27@gmail.com>
2020-06-24 17:36:48 -0400

    Try to fix request url in getAdminToken

--- bot/utils/getAdminToken.js
+++ bot/utils/getAdminToken.js
@@ -1,9 +1,10 @@
 const request = require("superagent");
 const config = require("../../config/serverInfo");
 module.exports = function getAdminToken(req, req, next) {
+  console.log("request url", `${req.domain}/api/user/login`);
   return request
     .post(`${req.domain}/api/user/login`)
-    .send({ username: "test_admin", password: "wrong_password" })
+    .send({ username: "test_admin", password: config.ADMIN_PASS })
     .then(r => r.body.token)
     .catch(next);
 };
