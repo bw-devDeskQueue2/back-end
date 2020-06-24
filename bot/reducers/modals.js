cfde5c8ad86ea373889eb13eeef33cf6492cfdf5
David Horstman <davidzilla27@gmail.com>
2020-06-24 16:12:15 -0400

    Work on automating modal reducer

--- bot/reducers/modals.js
+++ bot/reducers/modals.js
@@ -1,5 +1,16 @@
+const path = require("path");
+const fs = require("fs");
 const { modal: roles } = require("../handlers/roles");
 
-module.exports = {
-  roles,
-};
+let modals = { roles };
+const directory = path.join(__dirname, "..", "handlers");
+fs.readdirSync(directory, (err, files) => {
+  if (err) {
+    return console.error("Unable to read " + directory);
+  }
+  files.forEach(file => {
+    console.log("file", file);
+  });
+});
+
+module.exports = modals;
