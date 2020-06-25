e7f84422b772d4263d5712d848129deac342d3ac
David Horstman <davidzilla27@gmail.com>
2020-06-25 00:55:29 -0400

    Add error handling

--- bot/slackUserModel.js
+++ bot/slackUserModel.js
@@ -6,8 +6,12 @@ function getUser(query) {
     .then(results => (results.length === 0 ? null : results[0]));
 }
 
-//user: { slack_id, team_id, user_id }
 function addUser(user) {
+  const { slack_id, team_id, user_id } = user;
+  if (!slack_id && team_id && user_id) {
+    console.log("Error: Cannot add a slack user without all required fields");
+    return null;
+  }
   return knex("slack_users")
     .insert(user)
     .then(() => getUser(user));
