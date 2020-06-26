0bf8a60b15ec0af97d30081b14712f4cc18dede9
David Horstman <davidzilla27@gmail.com>
2020-06-26 01:11:51 -0400

    Try using urlencoded slack endpoints

--- bot/utils/slackUtils.js
+++ bot/utils/slackUtils.js
@@ -1,11 +1,23 @@
 const request = require("superagent");
 const config = require("../../config/serverInfo");
+const { encode } = require("querystring");
 
 const slackRequest = (body, endpoint, token = config.OAUTH_ACCESS_TOKEN) =>
   request
     .post(`https://slack.com/api/${endpoint}`)
     .send(body)
     .set("Authorization", `Bearer ${token}`)
+    .then(({ body }) => {
+      if (!body.ok) {
+        console.log("Error with request to " + endpoint, body);
+      }
+      return body;
+    })
+    .catch(console.error);
+
+const slackUrlEncodedRequest = (body, endpoint) =>
+  request
+    .post(`https://slack.com/api/${endpoint}?${encode(body)}`)
     .then(({ body }) => {
       if (!body.ok) {
         console.log("Error with request to " + endpoint, body);
@@ -36,21 +48,31 @@ const sendDM = (users, message) =>
   );
 
 const openChannel = (user_ids, message, name) =>
-  slackRequest(
-    { user_ids, name, is_private: true },
+  slackUrlEncodedRequest(
+    { name, token: config.BOT_ACCESS_TOKEN },
     "conversations.create",
     config.BOT_ACCESS_TOKEN
-  ).then(({ channel: { id: channelID } }) =>
-    slackRequest(
-      {
-        username: config.BOT_USERNAME,
-        channel: channelID,
-        token: config.BOT_ACCESS_TOKEN,
-        text: message,
-      },
-      "chat.postMessage",
-      config.BOT_ACCESS_TOKEN
+  )
+    .then(({ channel: { id: channelID } }) =>
+      slackUrlEncodedRequest(
+        {
+          channel: channelID,
+          user_ids,
+          token: config.BOT_ACCESS_TOKEN,
+        },
+        "conversations.invite"
+      )
     )
-  );
+    .then(({ channel: { id: channelID } }) =>
+      slackUrlEncodedRequest(
+        {
+          username: config.BOT_USERNAME,
+          channel: channelID,
+          token: config.BOT_ACCESS_TOKEN,
+          text: message,
+        },
+        "chat.postMessage"
+      )
+    );
 
 module.exports = { openView, pushView, sendDM, openChannel };
