62b70dbb2db906bddca17b4e2a03953a79f66b4e
David Horstman <davidzilla27@gmail.com>
2020-06-25 23:45:10 -0400

    Try to fix Promise.all concat issue

--- bot/handlers/ticketQueue.js
+++ bot/handlers/ticketQueue.js
@@ -81,42 +81,45 @@ async function followUpModal(ticket_id, req) {
     type: "modal",
     title: {
       type: "plain_text",
-      text: "Help with " + subject,
+      text: subject,
     },
-    blocks: await Promise.all(
-      messages.map(async ({ body, sender: { username, id } }, idx) => {
-        const slackUser = await SlackUsers.getUser({ user_id: id });
-        return {
-          type: "section",
-          block_id: `ticket_${id}_message_${idx}`,
-          text: {
-            type: "mrkdwn",
-            text: `*${
-              slackUser ? `<@${slackUser.slack_id}>` : username
-            }*\n${body}`,
-          },
-        };
-      })
-    )
-    .concat([
-      {
-        type: "input",
-        block_id: "message_body",
-        label: {
-          type: "plain_text",
-          text: "Your Response",
-        },
-        element: {
-          action_id: "body",
-          type: "plain_text_input",
-          multiline: true,
-          placeholder: {
+    blocks: []
+      .concat(
+        await Promise.all(
+          messages.map(async ({ body, sender: { username, id } }, idx) => {
+            const slackUser = await SlackUsers.getUser({ user_id: id });
+            return {
+              type: "section",
+              block_id: `ticket_${id}_message_${idx}`,
+              text: {
+                type: "mrkdwn",
+                text: `*${
+                  slackUser ? `<@${slackUser.slack_id}>` : username
+                }*\n${body}`,
+              },
+            };
+          })
+        )
+      )
+      .concat([
+        {
+          type: "input",
+          block_id: "message_body",
+          label: {
             type: "plain_text",
-            text: "Respond to help this user with their issue",
+            text: "Your Response",
+          },
+          element: {
+            action_id: "body",
+            type: "plain_text_input",
+            multiline: true,
+            placeholder: {
+              type: "plain_text",
+              text: "Respond to help this user with their issue",
+            },
           },
         },
-      },
-    ]),
+      ]),
     close: {
       type: "plain_text",
       text: "Cancel",
