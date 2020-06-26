6cfbddb632ef8ed2cd005e4304731c73e5cbbc36
David Horstman <davidzilla27@gmail.com>
2020-06-25 23:01:45 -0400

    Refactor sendDM to use slackRequest

--- bot/utils/slackUtils.js
+++ bot/utils/slackUtils.js
@@ -10,6 +10,7 @@ const slackRequest = (body, endpoint) =>
       if (!body.ok) {
         console.log("Error opening view", body);
       }
+      return body;
     })
     .catch(console.error);
 
@@ -20,33 +21,47 @@ const pushView = (trigger_id, view) =>
   slackRequest({ trigger_id, view }, "views.push");
 
 const sendDM = (users, message) =>
-  request
-    .post("https://slack.com/api/conversations.open")
-    .send({ users })
-    .set("Authorization", `Bearer ${config.BOT_ACCESS_TOKEN}`)
-    .then(({ body }) => {
-      if (!body.ok) {
-        return console.log("opening error", body);
-      }
-      const {
-        channel: { id: channelID },
-      } = body;
-      return request
-        .post("https://slack.com/api/chat.postMessage")
-        .set("Authorization", `Bearer ${config.BOT_ACCESS_TOKEN}`)
-        .send({
-          username: config.BOT_USERNAME,
-          channel: channelID,
-          token: config.BOT_ACCESS_TOKEN,
-          text: message,
-        })
-        .then(({ body }) => {
-          if (!body.ok) {
-            console.log("sending error", body);
-          }
-          //console.log("sent", body);
-        });
-    })
-    .catch(console.error);
+  slackRequest({ users }, "conversations.open").then(body => {
+    const {
+      channel: { id: channelID },
+    } = body;
+    return slackRequest(
+      {
+        username: config.BOT_USERNAME,
+        channel: channelID,
+        token: config.BOT_ACCESS_TOKEN,
+        text: message,
+      },
+      "chat.postMessage"
+    );
+  });
+// request
+//   .post("https://slack.com/api/conversations.open")
+//   .send({ users })
+//   .set("Authorization", `Bearer ${config.BOT_ACCESS_TOKEN}`)
+//   .then(({ body }) => {
+//     if (!body.ok) {
+//       return console.log("opening error", body);
+//     }
+//     const {
+//       channel: { id: channelID },
+//     } = body;
+//     return request
+//       .post("https://slack.com/api/chat.postMessage")
+//       .set("Authorization", `Bearer ${config.BOT_ACCESS_TOKEN}`)
+//       .send({
+//         username: config.BOT_USERNAME,
+//         channel: channelID,
+//         token: config.BOT_ACCESS_TOKEN,
+//         text: message,
+//       })
+//       .then(({ body }) => {
+//         if (!body.ok) {
+//           console.log("sending error", body);
+//         }
+//         //console.log("sent", body);
+//       });
+//   })
+//   .catch(console.error);
 
 module.exports = { openView, pushView, sendDM };
