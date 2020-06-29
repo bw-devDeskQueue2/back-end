8fc7f3de66b543dceecc907ebca9fef40ab1f223
David Horstman <davidzilla27@gmail.com>
2020-06-29 12:09:34 -0400

    Forgot to update PORT in console.log

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./server.js');
 const config = require("./config/serverInfo");
 
 server.listen(config.PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+  console.log(`\n=== Server listening on port ${config.PORT} ===\n`);
 });
