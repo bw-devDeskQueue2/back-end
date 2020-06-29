ba3656663c09c0ed8cd2c1944ddc6131f9b7c186
David Horstman <davidzilla27@gmail.com>
2020-06-29 17:16:31 -0400

    Delete old references to channel before adding new one

--- bot/slackChannelsModel.js
+++ bot/slackChannelsModel.js
@@ -1,10 +1,15 @@
 const knex = require("../data/dbConfig");
+const { where } = require("../data/dbConfig");
 
-function addChannel(channel) {
+async function addChannel(channel) {
   const { channel_id, team_id, name } = channel;
   if (!(channel_id && team_id && name)) {
     return new Error("Cannot add channel without all required info");
   }
+  await knex("open_private_channels")
+    .where({ team_id, name })
+    .delete()
+    .catch(console.log);
   return knex("open_private_channels")
     .insert({ channel_id, team_id, name })
     .then(() => findChannel({ channel_id, team_id, name }));
