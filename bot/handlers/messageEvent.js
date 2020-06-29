7b22973dbde766df9fe4d68ec923865caace949e
David Horstman <davidzilla27@gmail.com>
2020-06-28 22:03:23 -0400

    Also detect close inside backticks

--- bot/handlers/messageEvent.js
+++ bot/handlers/messageEvent.js
@@ -1,12 +1,17 @@
-function messageEvent(messageText, channel, slackUser) {
-  const channelSplit = channel.name ? channel.name.split("_") : ["no", "no"];
-  if (!(channelSplit[0] === "ddq" && channelSplit[1] === "ticket")) {
-    return; //console.log("Message not in a ticket channel");
+async function messageEvent(messageText, channel, slackUser) {
+  try {
+    const channelSplit = channel.name ? channel.name.split("_") : ["no", "no"];
+    if (!(channelSplit[0] === "ddq" && channelSplit[1] === "ticket")) {
+      return; //console.log("Message not in a ticket channel");
+    }
+    const ticket_id = channelSplit[2];
+    if (messageText === "!close"||messageText==="`!close`") {
+      return console.log("Close command sent");
+    }
+    console.log(messageText);
+  } catch (e) {
+    console.log(e);
   }
-  const ticket_id = channelSplit[2];
-  if (messageText === "!close") {
-    return console.log("Close command sent");
-  }
-  console.log(messageText);
 }
+
 module.exports = { messageEvent };
