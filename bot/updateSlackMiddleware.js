1f9c2e1f43509ce26c0f65aafffb32e8ad129a34
David Horstman <davidzilla27@gmail.com>
2020-06-29 12:24:38 -0400

    Forgot to copy over ticketId

--- bot/updateSlackMiddleware.js
+++ bot/updateSlackMiddleware.js
@@ -1,15 +1,28 @@
-const { closeChannel, findChannelByName } = require("./utils");
+const { closeChannel, findChannelByName, getMembers } = require("./utils");
 const SlackUsers = require("./slackUserModel");
 
 async function closeSlackChannelIfNecessary(req, res, next) {
   const { initiated_by_slackbot } = req.body;
+  const { ticketId } = req.params;
   if (initiated_by_slackbot) {
     return next();
   }
   try {
     const channel = await findChannelByName(`ddq_ticket_${ticketId}`);
     console.log("Slack channel to close\n", channel);
-    next();
+    // if (!channel) return next();
+    // const channelMembers = await getMembers(
+    //   channel.id
+    // ).then(({ ok, members }) => (ok ? members : []));
+    // const channelResponse = await closeChannel(channel.id);
+    // channelMembers.map(id =>
+    //   sendDM(
+    //     id,
+    //     `Ticket #${ticketId} was successfully ${
+    //       req.originalUrl.includes("close") ? "closed" : "unassigned"
+    //     }.`
+    //   ).catch(() => null)
+    // );
   } catch (e) {
     next(e);
   }
