769414215ba288238262e4ea1c68a7277104ce45
David Horstman <davidzilla27@gmail.com>
2020-06-29 15:40:07 -0400

    Remove old postInChannel

--- bot/handlers/messageEvent.js
+++ bot/handlers/messageEvent.js
@@ -42,11 +42,6 @@ async function messageEvent(messageText, channel, slackUser, req) {
     // );
     //Add ticket-related messages to the database
 
-    postInChannel(
-      channel.id,
-      "Message received and added to the ticket!"
-    );
-
     const user = await createUserIfNotExists(slackUser, req);
     const addedMessage = await request
       .post(`${baseURL(req)}/tickets/${ticket_id}/messages`)
